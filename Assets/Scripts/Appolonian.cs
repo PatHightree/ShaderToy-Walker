4dc5251ebcd46e29698e3fe92ebc699cce9b7bdc
Patrick Hogenboom <patrick@fultonia.nl>
2015-05-09 23:21:02 +0200

    Added world scale parameter.

--- Assets/Scripts/Appolonian.cs
+++ Assets/Scripts/Appolonian.cs
@@ -6,9 +6,11 @@ public class Appolonian : MonoBehaviour {
 
 	public Material material;
 	public RenderTexture OutputRT;
+	[Range(0, 1)]
+	public float WorldScale=0.5f;
 
 	void Update () {
-		material.SetVector("iCamPos", transform.position);
+		material.SetVector("iCamPos", transform.position * WorldScale);
 		material.SetVector("iCamRight", transform.right);
 		material.SetVector("iCamUp", transform.up);
 		material.SetVector("iCamForward", transform.forward);
