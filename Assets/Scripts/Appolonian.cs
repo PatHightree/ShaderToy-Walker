d910d9188d3a45527ac2879c12060ece32c5e52e
Patrick Hogenboom <patrick@fultonia.nl>
2015-05-10 00:46:01 +0200

    Added a simple gui that is toggled by double clicking. Allows you to modify the world scale (is not stored between sessions).

--- Assets/Scripts/Appolonian.cs
+++ Assets/Scripts/Appolonian.cs
@@ -6,8 +6,12 @@ public class Appolonian : MonoBehaviour {
 
 	public Material material;
 	public RenderTexture OutputRT;
-	[Range(0, 1)]
+	[Range(0.0001f, 1)]
 	public float WorldScale=0.5f;
+
+	public void OnValueChanged(float worldScale) {
+		WorldScale = worldScale;
+	}
 
 	void Update () {
 		material.SetVector("iCamPos", transform.position * WorldScale);
---
+++ Assets/Scripts/GUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI : MonoBehaviour {
+	public float DoubleClickInterval = 0.3f;
+	private float _lastClickTime = -1;
+	private bool _areSettingsVisible;
+	private GameObject _settingsPanel;
+	
+	public void Start() {
+		_settingsPanel = GameObject.FindGameObjectWithTag("Settings panel");
+		_settingsPanel.SetActive(false);
+	}
+
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			if (Time.time - _lastClickTime < DoubleClickInterval) {
+				//double click
+				print("done:" + (Time.time - _lastClickTime).ToString());
+				_areSettingsVisible = !_areSettingsVisible;
+				_settingsPanel.gameObject.SetActive(_areSettingsVisible);
+			} else {
+				//normal click
+				print("miss:" + (Time.time - _lastClickTime).ToString());
+			}
+			_lastClickTime = Time.time;
+		}	
+	}
+}
